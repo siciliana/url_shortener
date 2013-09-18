7bfb72759c48205b9a98b65a8c804373f7117ab0
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-18 11:30:54 -0500

    validations, rendered short_link

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,15 +3,31 @@ get '/' do
   erb :index 
 end
 
+def generate_shortlink(length=6)
+  chars = 'abcdefghijklmnopqrstuvwxyzabABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+  shortlink = ''
+  length.times { |char| shortlink << chars[rand(chars.length)] }
+  p shortlink
+end 
+
 post '/urls' do
   puts params
-  # initiate counter at 0
-  # take input and create new item in links table 
-  # generate shortlink
+  puts user_link = params[:link]
+  puts @user_input = user_link[:original_link]
+
+  @shortlink = generate_shortlink
+  # create instance of a link record 
+  link_record = Link.new
+  
   erb :urls
 end
 
 # e.g., /q6bda
 get '/:short_url' do
-  # redirect to appropriate "long" URL
+  # query the database to find original_link based on @shortlink
+  # increment counter 
+  # redirect to original_link
 end
+
+
+
